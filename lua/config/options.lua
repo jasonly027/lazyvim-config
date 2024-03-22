80a5def4c081fed7434f8f2a03b58b3cb6c7f291
Jason Ly <81354745+jasonly027@users.noreply.github.com>
2024-03-22 16:23:04 -0700

    Initial Config

---
+++ lua/config/options.lua
@@ -0,0 +1,12 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+local opt = vim.opt
+
+opt.guifont = "JetBrainsMonoNL Nerd Font Mono:h16"
+
+opt.shiftwidth = 4
+opt.tabstop = 4
+
+opt.scrolloff = 8
